14908f398da023dbad8a80c4443dcf06a01336f9
Sergio Asenjo <sergio.asenjo@usach.cl>
2021-11-29 17:06:26 -0300

    initial commit

---
+++ CapaGUI/PantallaControlCalidad.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace CapaGUI
+{
+    public partial class PantallaControlCalidad : Form
+    {
+        public PantallaControlCalidad()
+        {
+            InitializeComponent();
+        }
+    }
+}
